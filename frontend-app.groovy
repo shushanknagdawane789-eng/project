dfeacc9ccec08029fd3cdb480c96967eafd49d44
shushanknagdawane789-eng <shushanknagdawane789@gmail.com>
2026-07-25 13:39:15 +0000

    Add frontend-app.groovy

---
+++ frontend-app.groovy
@@ -0,0 +1,27 @@
+pipeline{
+    agent any
+    stages{
+        stage('Code-Pull'){
+            steps{
+                git branch: 'main', url: 'https://github.com/shushanknagdawane789-eng/project.git'    
+            }
+        }
+        stage('Code-Build'){
+            steps{
+                sh '''
+                    npm install
+                    npm run build
+                '''
+            }
+        }
+        stage('Deploy'){
+            steps{
+                withCredentials([aws(accessKeyVariable: 'AWS_ACCESS_KEY_ID', credentialsId: 'aws_creds', secretKeyVariable: 'AWS_SECRET_ACCESS_KEY')]) {
+                    sh '''
+                       aws s3 sync dist/ s3://cblkdfdnewcjdnd-project-bux/ 
+                    '''
+                } 
+            }
+        }
+    }
+}
