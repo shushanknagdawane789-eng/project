f84fd96ffa8fdfef7c8549fbe49c7ae2e45293ba
Shushank Nagdawane <shushanknagdawane789@gmail.com>
2026-07-25 20:33:23 +0530

    Fix S3 bucket name in deployment script

--- frontend-app.groovy
+++ frontend-app.groovy
@@ -18,7 +18,7 @@ pipeline{
             steps{
                 withCredentials([aws(accessKeyVariable: 'AWS_ACCESS_KEY_ID', credentialsId: 'aws_creds', secretKeyVariable: 'AWS_SECRET_ACCESS_KEY')]) {
                     sh '''
-                       aws s3 sync dist/ s3://cblkdfdnewcjdnd-project-bux/ 
+                       shushank-project-bucket-2026
                     '''
                 } 
             }
